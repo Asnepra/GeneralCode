a0c85f010356484d1219f1b93f29bb6faf9ff20b
Asnepra <ankitpratap1995@gmail.com>
2023-09-14 15:18:16 +0530

    Country Catgeories Added

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { Categories } from "@components/Categories";
 import { SearchInput } from "@components/search-input";
 import axios from "axios";
 
@@ -6,18 +7,20 @@ import axios from "axios";
  * @returns This is a server component that has access to DB
  */
 const RootPage = async () => {
+  var countryCategories = [];
   try {
     const response = await axios.get(
       "http://localhost:3000/api/country_category"
     ); // Specify the correct URL with port 3000
-    const countryCategories = response.data;
-    console.log(countryCategories);
+    countryCategories = response.data;
+    //console.log(countryCategories);
   } catch (e) {
     console.log(e);
   }
   return (
     <div className="h-full p-4 pl-24 space-y-2">
       <SearchInput />
+      <Categories data={countryCategories} />
     </div>
   );
 };
--- app/api/country_category/route.ts
+++ app/api/country_category/route.ts
@@ -10,12 +10,16 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
     //await connectMongoDb();
     await mssqlconnect();
     //Fetch all the countries data
-    const result = await sql.query`SELECT COUNTRY_NAME FROM Country_Master`;
-    const countryNames = result.recordset.map((record: { COUNTRY_NAME: string; }) => record.COUNTRY_NAME);
+    const result = await sql.query`SELECT Country_Id AS id, COUNTRY_NAME FROM Country_Master`;
+    // Map the result to a JSON format with id and COUNTRY_NAME
+    const countryData = result.recordset.map((record: { id: number; COUNTRY_NAME: string; }) => ({
+      id: record.id,
+      COUNTRY_NAME: record.COUNTRY_NAME
+    }));
 
     //console.log(result);
 
-    return new NextResponse(JSON.stringify(countryNames), { status: 200, headers: { 'Content-Type': 'application/json' } });
+    return new NextResponse(JSON.stringify(countryData), { status: 200, headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
     // Handle errors and send an error response with status code 500
     return new NextResponse("IN Api Calling error internal Error", { status: 500 });
